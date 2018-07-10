43b8d0293e7432ef0cb872b507ca87d203970d0b
Travis Tokuyama <travistokuyama@gmail.com>
2018-07-10 14:50:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,4 +28,4 @@ var funkyFunction = function funk1() {
 // NOTE: you only need to modify the code below this line.
 var theFunk = funkyFunction();
 theFunk()
-console.log(theFunk)
+console.log(theFunk())
